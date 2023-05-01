bfa6b73e66c117933f350c95f156242a0354ea42
Ontiimeyes <lucassalinroge@gmail.com>
2023-05-01 13:14:14 +0200

    In progress

--- ex02/includes/PmergeMe.hpp
+++ ex02/includes/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: lsalin <lsalin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 12:37:36 by lsalin            #+#    #+#             */
-/*   Updated: 2023/04/29 12:44:49 by lsalin           ###   ########.fr       */
+/*   Updated: 2023/05/01 13:02:43 by lsalin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@
 class PmergeMe
 {
 	private:
-		std::vector<int> vector_;
-		std::deque<int>	deque_;
+		std::vector<int>	_vector;
+		std::deque<int>		_deque;
 
 	public:
 		PmergeMe();
@@ -36,44 +36,77 @@ class PmergeMe
 		void	printVector();
 		void	printDeque();
 
+		// Implementation de l'algo de tri par fusion
+		// @param lst : reference a un container (vector ou deque) contenant les elements a trier
+
 		template< typename T >
-		void	merge_sort(T & lst, int beg, int end)
+		void	merge_sort(T &lst, int beg, int end)
 		{
-			int mid;
+			int	mid;
 
 			if (beg < end)  
 			{
-				mid = (beg+end)/2;
+				mid = (beg + end) / 2;
 
-				merge_sort(lst, beg, mid);  
-				merge_sort(lst, mid+1, end);  
-				merge(lst, beg, mid, end);  
+				merge_sort(lst, beg, mid); // tri recursif de la premiere moitie du tableau initial
+				merge_sort(lst, mid + 1, end); // deuxieme moitie
+				merge(lst, beg, mid, end);
 			}
 		}
 
-	template< typename T >
-	void	merge(T &lst, int left, int mid, int right)
-	{  
-		std::vector<int>	temp(right - left + 1);
-		int					i = left, j = mid+1, k = 0;
+		// Fusionne deux sous-tableaux tries adjacents
+		// 1er sous-tableau : left-mid
+		// 2e sous-tableau	: mid-right
 
-		while (i <= mid && j <= right)
+		template< typename T >
+		void	merge(T &lst, int left, int mid, int right)
 		{
-			if (lst[i] <= lst[j])
+			std::vector<int>	temp(right - left + 1); // stocke temporairement les elements fusionnes
+			int					i = left				// premier element du premier sous-tableau
+			int					j = mid + 1 			// premier element du deuxieme sous-tableau
+			int					k = 0;					// index pour inserer des elements dans temp
+
+			/**
+				@example lst = [3, 5, 8, 9, 1, 4, 6, 7]
+				
+				left = 0
+				right = 7
+				mid = 3
+
+				1er sous-tableau : [3, 5, 8, 9]
+				2e sous-tableau	 : [1, 4, 6, 7]
+
+				- i = 0 (pointe sur 2)
+				- j = 4 (pointe sur 1)
+				- k = 0
+			*/
+
+			// Fusionne les elements des deux sous-tableaux
+			// Et les copie de facon croissante dans tmp
+			// S'arrete lorsque l'un des deux sous-tableaux est copie trie dans tmp
+
+			while (i <= mid && j <= right)
+			{
+				if (lst[i] <= lst[j]) // si 2 < 1
+					temp[k++] = lst[i++];
+				else
+					temp[k++] = lst[j++];
+			}
+			
+			// temp = [1, 3, 4, 5]
+
+			// Copie les elements restants du premier sous-tableau (left-mid) dans tmp
+			// Arrive si des elements du 2e sous-tableau sont < aux elements restants du premier sous-tableau
+
+			while (i <= mid)
 				temp[k++] = lst[i++];
-			else
+
+			while (j <= right)
 				temp[k++] = lst[j++];
+
+			for (int p = 0; p < k; p++)
+				lst[left + p] = temp[p];
 		}
-		
-		while (i <= mid)
-			temp[k++] = lst[i++];
-
-		while (j <= right)
-			temp[k++] = lst[j++];
-
-		for (int p = 0; p < k; p++)
-			lst[left+p] = temp[p];
-	}
 };
 
 #endif
--- ex02/srcs/PmergeMe.cpp
+++ ex02/srcs/PmergeMe.cpp
@@ -6,8 +6,109 @@
 /*   By: lsalin <lsalin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 12:37:34 by lsalin            #+#    #+#             */
-/*   Updated: 2023/04/29 12:45:33 by lsalin           ###   ########.fr       */
+/*   Updated: 2023/05/01 13:14:06 by lsalin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe() {}
+
+PmergeMe::~PmergeMe() {}
+
+PmergeMe::PmergeMe(const PmergeMe &origin)
+{
+	*this = origin; 
+}
+
+PmergeMe    &PmergeMe::operator=(const PmergeMe &origin)
+{
+	this->_vector = origin._vector;
+	this->_deque = origin._deque;
+	
+	return (*this);
+}
+
+void	PmergeMe::printVector()
+{
+	int i = 0;
+
+	for (std::vector<int>::iterator it = _vector.begin(); it < _vector.end(); it++)
+	{
+		if (++i > 8)
+		{
+			std::cout << " [...]";
+			break;
+		}
+		std::cout << " " << *it;
+	}
+	std::cout << std::endl;
+}
+
+void	PmergeMe::printDeque()
+{
+	int i = 0;
+
+	for (std::deque<int>::iterator it = _deque.begin(); it != _deque.end(); it++)
+	{
+		if (++i > 8)
+		{
+			std::cout << " [...]";
+			break;
+		}
+		std::cout << " " << *it;
+	}
+	std::cout << std::endl;
+}
+
+void	is_Num(char *tab)
+{
+	std::string	str = tab;
+
+	if (str[0] == '-')
+		throw "Error: not a positive number.";
+
+	if (str.length() > 10 || (str.length() == 10 && str > "2147483647"))
+		throw "Error: too large a number.";
+
+	if (!str.empty() && str.find_first_not_of("0123456789") != std::string::npos)
+		throw "Error: not a number.";
+}
+
+void	PmergeMe::ft_sort(char **tab)
+{
+	std::clock_t	start;
+	std::clock_t	end;
+	double			Vtime;
+	double			Dtime;
+	int				i = 1;
+	int				nb = 0;
+
+	while (tab[i])
+	{
+		is_Num(tab[i]);
+		std::istringstream(tab[i]) >> nb;
+
+		_vector.push_back(nb);
+		_deque.push_back(nb);
+
+		i++;
+	}
+
+	std::cout << "Before :";
+	printVector();
+	start = clock();
+	merge_sort(_vector, 0, _vector.size() - 1);
+	end = clock();
+	Vtime = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000;
+	start = clock();
+	merge_sort(_deque, 0, _vector.size() - 1);
+	end = clock();
+	Dtime = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000;
+	std::cout << "After :";
+	printVector();
+	//printDeque();
+	std::cout << std::fixed << std::setprecision(3);
+	std::cout << "Time to process a range of "<< _vector.size() <<  " elements with std::vector : " << Vtime << " ms\n";
+	std::cout << "Time to process a range of "<< _deque.size() <<  " elements with std::list : " << Dtime << " ms\n";
+}
